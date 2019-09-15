f509917cb20a6ccfa4d8b848563e2d26c4692e6e
Eric Bai <eric@ericbai.com>
2019-09-15 15:58:51 -0400

    finished initial desktop styling, next is making site responsive

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import SlickCarousel from 'slick-carousel';
+import $ from 'jquery';
+import './index.scss';
+
+$(function() {
+  $('[data-slick]')
+    .on('init reInit', ({ target }, { currentSlide }) => {
+      setCategoryActive(target, currentSlide);
+    })
+    .on('beforeChange', ({ target }, slick, currentSlide, nextSlide) => {
+      setCategoryActive(target, nextSlide);
+    })
+    .slick();
+
+  $('[data-slick-nav-target-id][data-slick-nav-target-index]').on(
+    'click',
+    function() {
+      const $this = $(this),
+        targetSelector = `#${$this.data('slickNavTargetId')}`,
+        targetIndex = $this.data('slickNavTargetIndex');
+      $(targetSelector).slick('slickGoTo', targetIndex);
+    },
+  );
+});
+
+function setCategoryActive(sliderEl, index) {
+  const activeClass = sliderEl.dataset.slickActiveClass;
+  $(`[data-slick-nav-target-id=${sliderEl.id}]`)
+    .removeClass(activeClass)
+    .filter(`[data-slick-nav-target-index=${index}]`)
+    .addClass(activeClass);
+}
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'resolve-url-loader',
+                    { loader: 'sass-loader', options: { sourceMap: true } },
+                ],
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot|otf)$/i,
+                use: 'file-loader',
+            },
+            {
+                test: /\.(png|jpg)$/,
+                exclude: /node_modules/,
+                use: ['url-loader'],
+            },
+            // So that automatic image-requiring of `html-loader` can co-exist with
+            // the html pre-processing of `html-webpack-plugin`
+            // see: https://github.com/webpack-contrib/html-loader/issues/195#issuecomment-461315765
+            {
+                test: /\.ejs$/,
+                exclude: /node_modules/,
+                use: ['extract-loader', 'html-loader', 'ejs-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.ejs',
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+    ],
+}
