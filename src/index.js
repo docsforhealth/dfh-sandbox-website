5f2a1cc881cf3a68fb6088211e41d0cc899d3c9e
Eric Bai <eric@ericbai.com>
2019-11-27 19:39:38 -0500

    finished responsive toolkit detail page

--- src/index.js
+++ src/index.js
@@ -26,6 +26,16 @@ $(function() {
     $('.nav').toggleClass('nav--open');
     $('.nav__links-container').slideToggle('fast');
   });
+
+  $('.toolkit-topics__toggle').on('click', function() {
+    $('.toolkit-topics').toggleClass('toolkit-topics--open');
+    const $this = $(this),
+      oldText = $this.text(),
+      newText = oldText.includes('Show')
+        ? oldText.replace('Show', 'Hide')
+        : oldText.replace('Hide', 'Show');
+    $this.text(newText);
+  });
 });
 
 function setCategoryActive(sliderEl, index) {
